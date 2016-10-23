7d0b21848e6296e381c289bd344546e79ed723af
CARETTE Romain <rrcarette@gmail.com>
2016-10-23 20:56:55 +0200

    Norme get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rcarette <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/29 18:19:08 by rcarette          #+#    #+#             */
-/*   Updated: 2016/10/08 01:03:38 by rcarette         ###   ########.fr       */
+/*   Updated: 2016/10/23 20:56:14 by rcarette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 # define BUFF_SIZE 5
 # include <unistd.h>
 # include <stdlib.h>
+
 int		get_next_line(int const fd, char **line);
 #endif
