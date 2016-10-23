5480227104d884d0a7415158fa4c0fb93f522c42
CARETTE Romain <rrcarette@gmail.com>
2016-10-23 19:17:32 +0200

    Modification de code get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rcarette <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/29 18:22:04 by rcarette          #+#    #+#             */
-/*   Updated: 2016/10/08 02:32:23 by rcarette         ###   ########.fr       */
+/*   Updated: 2016/10/23 19:16:54 by rcarette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,6 @@ int		ft_getline(char **line, char **str)
 	{
 		if ((*str)[j] != '\n')
 			(*line)[i++] = (*str)[j++];
-		else if ((*str)[j] == '\0')
-			return (-1);
 		else if ((*str)[j] == '\n')
 		{
 			(*line)[i] = '\0';
@@ -38,25 +36,9 @@ int		ft_getline(char **line, char **str)
 			return (1);
 		}
 	}
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char		*str;
-	int			i;
-
-	i = -1;
-	str = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-	if (str)
-	{
-		while (*s1)
-			str[++i] = *s1++;
-		while (*s2)
-			str[++i] = *s2++;
-		str[++i] = '\0';
-		return (str);
-	}
-	return (NULL);
+	printf("Passage ici !");
+	free(*str);
+	return (0);
 }
 
 int		get_next_line(int const fd, char **line)
@@ -64,6 +46,7 @@ int		get_next_line(int const fd, char **line)
 	char			board[BUFF_SIZE];
 	size_t			ret;
     static char		*str = "";
+	char			*temporary;
 
 	ret = 0;
 	if (fd < 0 || BUFF_SIZE <= 0)
@@ -71,16 +54,38 @@ int		get_next_line(int const fd, char **line)
 	while ((ret = read(fd, board, BUFF_SIZE)))
 	{
 		board[ret] = '\0';
-		str = ft_strjoin(str, board);
+		if (*str == '\0')
+		{
+			str = (char *)malloc(sizeof(char) * strlen(board) + 1);
+			memcpy(str, board, strlen(board) + 1);
+		}
+		else
+		{
+			temporary = (char *)malloc(sizeof(char) * strlen(str) + 1);
+			memcpy(temporary, str, strlen(str) + 1);
+			free(str);
+			str = NULL;
+			str = malloc(sizeof(char) * strlen(temporary) + strlen(board) + 1);
+			memcpy(str,temporary, strlen(temporary) + 1);
+			strcat(str, board);
+			free(temporary);
+		}
 	}
+
 	return (ft_getline(line, &str));
 }
 
 int		main(int argc, char **argv)
 {
-	int		descriptor = open(argv[1], O_RDONLY);
+	int		descriptor = open("romainCarette", O_RDONLY);
 	char	*s1;
-	while (get_next_line(descriptor, &s1))
+	s1 = NULL;
+		
+		get_next_line(descriptor, &s1);
 		printf("%s", s1);
+		free(s1);
+		get_next_line(descriptor, &s1);
+		printf("%s", s1);
+		free(s1);
 	return (0);
 }
